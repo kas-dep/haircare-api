9717d92a6413066d460f7023d0526eb51b90e2e6
Katarzyna Depczyńska <kasiadepczynskapl@gmail.com>
2021-04-11 09:56:40 +0200

    change port - heroku

--- app.js
+++ app.js
@@ -25,8 +25,10 @@ app.use('/auth', authRouter);
 app.use('/coaDict', courseOfActiomDictRouter);
 app.use('/pcDict', productCategoryDict);
 
-app.listen(process.env.PORT, () => {
-  console.log('App is running');
+const port = process.env.PORT || '3000';
+
+app.listen(port, () => {
+  console.log(`App is running, port: ${port}`);
 });
 
 module.exports = app;
